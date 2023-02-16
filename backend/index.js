0f75b1c24de2061d1df115db30a2c31491573057
Muhammad Qasim <m.qasim162000@gmail.com>
2023-02-16 18:42:25 +0500

    MongoDB Set up successfully

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const connectToMongo = require("./db");
+
+const app = express();
+const port = 3000;
+
+connectToMongo();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
