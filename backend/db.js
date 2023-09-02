a8ae6a91bc6a1dd52b963a1297d6c6f426f7a0e8
Qasim162000 <m.qasim162000@gmail.com>
2023-09-02 17:21:13 +0500

    Mongodb atlas string added

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const server = "127.0.0.1:27017";
-const database = "travellog";
+// const server = "127.0.0.1:27017";
+// const database = "travellog";
 
 const connectToMongo = () => {
-  mongoose.connect(`mongodb://${server}/${database}`),
+  mongoose.connect(`mongodb+srv://mqasim162000:iamqasim@cluster0.weun7e9.mongodb.net/`),
     () => {
       console.log("Connected to MongoDB successfully");
     };
